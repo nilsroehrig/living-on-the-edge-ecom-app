367af32a819beff35f99de68811aed400ac059e5
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-06-27 14:45:52 +0200

    Initial commit with product data json and endpoint

---
+++ src/lib/sharedtypes/product.ts
@@ -0,0 +1,12 @@
+import type { Category } from './category'
+
+export interface Product {
+  id: string
+  name: string
+  description: string
+  price: number
+  brand: string
+  origin: string
+  category: Category
+  filename: string
+}
---
+++ src/routes/products/[id].svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { Product } from '$lib/sharedtypes/product'
+
+  export let product: Product
+</script>
+
+{#if product == null}
+  Das gewünschte Produkt wurde nicht gefunden.
+{:else}
+  Das gewünschte Produkt ist vorhanden.
+{/if}
