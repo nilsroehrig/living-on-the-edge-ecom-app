5da535d62b1bc84086732450358e2178a9c5bc0e
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-07-25 11:45:29 +0200

    add ssr support and footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts" context="module">
   import type { Load } from '@sveltejs/kit';
+  import Cart from '$lib/components/Cart.svelte';
+  import type { Category } from '$lib/sharedtypes/category';
+  import { Container, Group, SvelteUIProvider } from '@svelteuidev/core';
 
   export const load: Load = async ({ fetch }) => {
     const url = `/api/categories`;
@@ -14,31 +17,30 @@
 </script>
 
 <script lang="ts">
-  import Cart from '$lib/components/Cart.svelte';
-  import type { Category } from '$lib/sharedtypes/category';
-  import { Container, Group } from '@svelteuidev/core';
-
   export let categories: Category[];
 </script>
 
-<header>
-  <Container>
-    <Group position="apart">
-      <div class="brand">svekom</div>
-      <div class="cart"><Cart /></div>
-    </Group>
-  </Container>
-</header>
-<nav>
-  <Container>
-    <Group position="apart" align="center">
-      {#each categories as category}
-        <a href="/categories/{category.slug}">{category.name}</a>
-      {/each}
-    </Group>
-  </Container>
-</nav>
-<Container><slot /></Container>
+<SvelteUIProvider ssr>
+  <header>
+    <Container>
+      <Group position="apart">
+        <div class="brand">svekom</div>
+        <div class="cart"><Cart /></div>
+      </Group>
+    </Container>
+  </header>
+  <nav>
+    <Container>
+      <Group position="apart" align="center">
+        {#each categories as category}
+          <a href="/categories/{category.slug}">{category.name}</a>
+        {/each}
+      </Group>
+    </Container>
+  </nav>
+  <Container><slot /></Container>
+  <footer>&copy; 2022 Svekom Enterprises</footer>
+</SvelteUIProvider>
 
 <style>
   header,
@@ -64,5 +66,12 @@
 
   .cart {
     transform: translateY(2px);
+  }
+
+  footer {
+    box-shadow: inset 0 1px 0 0 #f0f0f0;
+    padding: 1rem;
+    text-align: center;
+    line-height: 1.5;
   }
 </style>
