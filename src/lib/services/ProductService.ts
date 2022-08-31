d120edf35b9160a0e2839e6263733d7c32480ac3
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-08-31 17:28:54 +0200

    get featured products from kv

--- src/lib/services/ProductService.ts
+++ src/lib/services/ProductService.ts
@@ -4,22 +4,41 @@ import type { CategorizedProduct, Product } from '$lib/domain/Product';
 import { Option } from 'prelude-ts';
 import { assoc, clone, filter, map, pipe, propEq } from 'ramda';
 
-export async function getMultipleProducts(ids: string[]): Promise<Product[]> {
-	const filtered = products.filter((product) => ids.includes(product.id));
-	return clone(filtered);
-}
-
-export async function getSingleProduct(id: string): Promise<Option<Product>> {
+export async function getSingleProduct (id: string): Promise<Option<Product>> {
 	return Option.ofNullable(
-		clone(products).find((product) => product.id === id)
+		clone(products).find((product) => product.id === id),
 	);
 }
 
-export async function getProductsByCategory(
-	category: Category
+export async function getProductsByCategory (
+	category: Category,
 ): Promise<CategorizedProduct[]> {
 	return pipe(
 		filter<Product>(propEq('category', category.id)),
-		map(assoc('category', category))
+		map(assoc('category', category)),
 	)(products);
+}
+
+export function createProductService (platform: Required<Readonly<App.Platform>>) {
+	const store = platform.env.PRODUCTS;
+
+	return {
+		async getMultipleProducts (ids: string[]): Promise<Product[]> {
+			const settled = await Promise.allSettled(
+				ids.map((id) => store.get<Product>(id, { type: 'json' })),
+			);
+			return settled.reduce((acc, it) => {
+				if (it.status === 'rejected') {
+					console.error(it.reason);
+					return acc;
+				}
+
+				if (it.value == null) {
+					return acc;
+				}
+
+				return acc.concat(it.value);
+			}, [] as Product[]);
+		},
+	};
 }
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,20 +1,31 @@
 import { getRandomCategory } from '$lib/services/CategoryService';
-import { getMultipleProducts } from '$lib/services/ProductService';
+import {
+	createProductService,
+} from '$lib/services/ProductService';
+import { error } from '@sveltejs/kit';
+import { isNil } from 'ramda';
 import type { PageServerLoad } from './$types';
 
-const featuredIds = [
-	'3560da91-ad6a-4586-922d-301b4b929871', // golden sunglasses
-	'ffc477fd-ac57-4856-836f-e40d54b01a66', // gbc
-	'350739db-c208-4f07-8e61-a292105a1a5f', // green tea serum
-	'cefccce0-9d15-4711-b662-0e73cf07619e', // watch
-	'859e1663-b7df-49e1-807e-71416ecdabbc', // ipad
-	'98acf652-8a3d-451e-9342-42f982db46e0', // kinn lotion
-];
+export const load: PageServerLoad = async ({ platform }) => {
+	if (isNil(platform?.env)) {
+		throw error(500);
+	}
 
-//@ts-ignore
-export const load: PageServerLoad = async () => {
+	const featuredIds: string[] | null = await platform.env.CONTENT.get(
+		'featuredIds',
+		{
+			type: 'json',
+		}
+	);
+
+	if (isNil(featuredIds)) {
+		throw error(500);
+	}
+
+	// @ts-ignore
+	const service = createProductService(platform);
 	return {
 		category: await getRandomCategory(),
-		products: await getMultipleProducts(featuredIds),
+		products: await service.getMultipleProducts(featuredIds),
 	};
 };
